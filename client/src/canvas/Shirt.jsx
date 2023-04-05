4b904f0c9f6881597a2456d78d53d418c372960f
Marcelo <marcelo011299@gmail.com>
2023-04-04 23:46:18 -0600

    3D Model instance

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -1,6 +1,25 @@
+import { easing } from 'maath'
+import { useFrame } from '@react-three/fiber';
+import { useGLTF, useTexture, Decal } from '@react-three/drei'
+import { useSnapshot } from 'valtio'
+import state from '../store'
+
 const Shirt = () => {
+    const snap = useSnapshot(state)
+    const { nodes, materials } = useGLTF('/shirt_baked.glb')
+
+    const logoTexture = useTexture(snap.logoDecal)
+    const fullTexture = useTexture(snap.fullDecal)
+
+
     return (
-        <div>Shirt</div>
+        <group>
+            <mesh castShadow
+                geometry={nodes.T_Shirt_male.geometry}
+                material={materials.lambert1}
+                material-roughness={1}
+                dispose={null} />
+        </group>
     )
 }
 export default Shirt
